bf0e366e17120633452bc7a65259633939bf4fa5
mattpake <pakematt@gmail.com>
2022-03-30 20:59:57 +0200

    search bar
    search bar, list view

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,16 +11,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 18),
+            child: AnimSearchBar(
+              width: 400,
+              textController: textController,
+              onSuffixTap: () {
+                setState(
+                  () {
+                    textController.clear();
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 1.5,
+            child: SingleChildScrollView(
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return const ListTile(
+                    leading: Icon(Icons.account_box_outlined),
+                    title: Text('Person name'),
+                  );
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
