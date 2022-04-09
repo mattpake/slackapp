32fd1e2eb3b5c0fa0d33aeada1bf899016e8c335
mattpake <pakematt@gmail.com>
2022-04-09 15:14:02 +0200

    search bar
    searching in list

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,12 +16,60 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController textController = TextEditingController();
 
-  Future<List<Member>> get members async => await fetchMembers();
+  List<Member> members = [];
+  List<Member> membersDuplicate = [];
+  bool isSearchResult = true;
 
   @override
   void initState() {
-    members;
+    getMembers();
     super.initState();
+  }
+
+  void getMembers() {
+    fetchMembers().then(
+      (value) {
+        setState(
+          () {
+            for (var item in value) {
+              members.add(item);
+              membersDuplicate.add(item);
+            }
+          },
+        );
+      },
+    );
+  }
+
+  void filterSearchResults(String query) {
+    if (query.isNotEmpty) {
+      List<Member> transferData = [];
+      for (var item in membersDuplicate) {
+        if (item.name.contains(query)) {
+          transferData.add(item);
+        } else {
+          setState(
+            () {
+              isSearchResult = false;
+            },
+          );
+        }
+      }
+      setState(
+        () {
+          members.clear();
+          members.addAll(transferData);
+        },
+      );
+      return;
+    } else {
+      setState(
+        () {
+          members.clear();
+          members.addAll(membersDuplicate);
+        },
+      );
+    }
   }
 
   @override
@@ -30,55 +78,72 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Member>>(
-        future: fetchMembers(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(18),
-                  child: TextField(
-                    controller: textController,
-                    decoration: const InputDecoration(
-                        labelText: "Search",
-                        hintText: "Search",
-                        prefixIcon: Icon(Icons.search),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: TextField(
+              onChanged: (value) {
+                filterSearchResults(value);
+              },
+              controller: textController,
+              decoration: const InputDecoration(
+                labelText: "Search",
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25.0),
                   ),
                 ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height / 1.5,
-                  child: SingleChildScrollView(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          leading: const Icon(Icons.account_box_outlined),
-                          title: Text(snapshot.requireData[index].name),
-                          onTap: () {
-                            Navigator.pushNamed(
-                              context,
-                              PersonScreen.routeName,
-                              arguments: ScreenArguments(widget.title, 'message'),
+              ),
+            ),
+          ),
+          Builder(
+            builder: (context) {
+              if (members.isNotEmpty) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 1.5,
+                      child: SingleChildScrollView(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.vertical,
+                          itemCount: members.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              leading: const Icon(Icons.account_box_outlined),
+                              title: Text(members[index].name),
+                              onTap: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  PersonScreen.routeName,
+                                  arguments: ScreenArguments(widget.title, 'message'),
+                                );
+                              },
                             );
                           },
-                        );
-                      },
-                    ),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              } else if (isSearchResult) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30.0),
+                    child: Text('No search result'),
                   ),
-                )
-              ],
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
   }
